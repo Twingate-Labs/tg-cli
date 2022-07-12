0ead8765e537e8de966a9d60efe7f8fbd3f3391f
chenbishop <chen.bishop@gmail.com>
2022-07-12 16:43:52 +0100

    fixing the issue with github release

--- cliCmd/deploy/index.mjs
+++ cliCmd/deploy/index.mjs
@@ -2,16 +2,17 @@ import {Command, EnumType} from "https://deno.land/x/cliffy/command/mod.ts";
 import {loadNetworkAndApiKey} from "../../utils/smallUtilFuncs.mjs";
 import {TwingateApiClient} from "../../TwingateApiClient.mjs";
 import {Log} from "../../utils/log.js";
-import {deployTerraformCommand} from "./terraform.mjs";
+// import {deployTerraformCommand} from "./terraform.mjs";
 
 
 export const deployCmd = new Command()
     .option("-y, --assume-yes [boolean]", "Automatic yes to prompts; assume 'yes' as answer to all prompts", {global: true})
     .description("Deploy Twingate to various targets")
+    .hidden()
     .action(async (options) => {
         const {networkName, apiKey} = await loadNetworkAndApiKey(options.accountName);
         options.apiKey = apiKey;
         options.client = new TwingateApiClient(networkName, apiKey, {logger: Log});
     });
 
-deployCmd.command("terraform", deployTerraformCommand);
+// deployCmd.command("terraform", deployTerraformCommand);

586597d2aa568999d31d4a0bd8cdd0473ce82213
chenbishop <chen.bishop@gmail.com>
2022-08-04 17:22:22 +0100

    added azure container deployment

--- cliCmd/deploy/az.mjs
+++ cliCmd/deploy/az.mjs
@@ -1,5 +1,6 @@
 import {Command} from "https://deno.land/x/cliffy/command/mod.ts";
 import {AzureVmDeployer} from "./deployers/azure/AzureVmDeployer.mjs";
+import {AzureContainerDeployer} from "./deployers/azure/AzureContainerDeployer.mjs";
 
 export const deployAzVmCommand = new Command()
     .description("Deploy Twingate on an Azure Virtual Machine")
@@ -8,6 +9,16 @@ export const deployAzVmCommand = new Command()
     })
     .action(async (options) => await (new AzureVmDeployer(options)).deploy());
 
+export const deployAzContainerCommand = new Command()
+    .description("Deploy Twingate on an Azure Container")
+    .option("-c, --cpu <String>", "Number of CPUs", {
+        default: "1"
+    })
+    .option("-m, --memory <String>", "Memory size in GB", {
+        default: "2"
+    })
+    .action(async (options) => await (new AzureContainerDeployer(options)).deploy());
+
 export const deployAzCommand = new Command()
     .description("Deploy Twingate on Microsoft Azure. Requires Azure CLI to be installed.")
     .globalOption(
@@ -15,4 +26,5 @@ export const deployAzCommand = new Command()
       "Subscription to use when interacting with Azure CLI."
     )
     .command("vm", deployAzVmCommand)
+    .command("container", deployAzContainerCommand)
 ;
--- cliCmd/deploy/deployers/azure/AzureContainerDeployer.mjs
+++ cliCmd/deploy/deployers/azure/AzureContainerDeployer.mjs
@@ -10,42 +10,30 @@ import {AzureBaseDeployer} from "./AzureBaseDeployer.mjs";
 export class AzureContainerDeployer extends AzureBaseDeployer {
 
 
-    async createContainer(resourceGroupName, vnetName, subnetName, name) {
-        /*
-        const cmd = this.getAzureCommand("vm", "create");
-        cmd.push("-g", resourceGroupName);
-        cmd.push("--vnet-name", vnetName);
+    async createContainer(resourceGroupName, vnetName, subnetName, name, options,accountUrl, tokens) {
+        const cmd = this.getAzureCommand("container", "create");
         cmd.push("--name", name);
-        cmd.push("--accept-term");
-        // See https://docs.microsoft.com/en-gb/azure/virtual-machines/automatic-vm-guest-patching#supported-os-images
-        // cmd.push("--image", "canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest");
-        cmd.push("--image", "canonical:0001-com-ubuntu-server-focal:20_04-lts:latest");
-        //cmd.push("--image", "canonical:0001-com-ubuntu-minimal-focal:minimal-20_04-lts:latest");
-        cmd.push("--custom-data", customData);
-        cmd.push("--enable-hotpatching");
-        cmd.push("--patch-mode", "AutomaticByPlatform");
-        cmd.push("--size", size);
-        if ( keyName === "" ) {
-            cmd.push("--generate-ssh-keys");
-        }
-        else if ( typeof keyName === "string" ) {
-            cmd.push("--ssh-key-name", keyName);
-        }
-        cmd.push("--subnet", subnetName);
-        cmd.push("--tags", "Service=twingate-connector");
-        if ( assignPublicIp === false ) {
-            cmd.push("--public-ip-address", "");
-        }
-        else {
-            cmd.push("--public-ip-sku", "Standard");
-        }
-        cmd.push("--nic-delete-option", "Delete");
-        cmd.push("--os-disk-delete-option", "Delete");
-        //cmd.push("--ephemeral-os-disk");
-        //cmd.push("--ephemeral-os-disk-placement", "CacheDisk");
-        cmd.push("--nsg", "twingate-connectorNSG");
-        cmd.push("--nsg-rule", "NONE");
-        */
+        cmd.push("--image", "twingate\/connector:1");
+        cmd.push("--resource-group", resourceGroupName);
+        cmd.push("--vnet", vnetName);
+        cmd.push("--subnet", subnetName)
+        cmd.push("--cpu", options.cpu)
+        cmd.push("--memory", options.memory)
+
+        // let envString =`TENANT_URL=${accountUrl} ACCESS_TOKEN=${tokens.accessToken} REFRESH_TOKEN=${tokens.refreshToken} TWINGATE_TIMESTAMP_FORMAT=2 TWINGATE_LABEL_DEPLOYEDBY=tgcli-az-acs`;
+        // cmd.push("--environment-variables", envString);
+        cmd.push("--environment-variables")
+        cmd.push(`TENANT_URL=${accountUrl}`)
+        cmd.push(`ACCESS_TOKEN=${tokens.accessToken}`)
+        cmd.push(`REFRESH_TOKEN=${tokens.refreshToken}`)
+        cmd.push(`TWINGATE_TIMESTAMP_FORMAT=2`)
+        cmd.push(`TWINGATE_LABEL_DEPLOYEDBY=tgcli-az-acs`)
+
+
+        const output = await execCmd(cmd);
+        let vnets = JSON.parse(output);
+        return vnets;
+
     }
 
     async deploy() {
@@ -59,21 +47,17 @@ export class AzureContainerDeployer extends AzureBaseDeployer {
             vnet = await this.selectVirtualNetwork(resourceGroup.name),
             subnet = await this.selectSubnet(vnet.subnets),
             hostname = `tg-${connector.name}`,
-            accountUrl = `https://${this.cliOptions.accountName}.twingate.com`,
-            tokens = await this.client.generateConnectorTokens(connector.id)
-        ;
+            accountUrl = !this.cliOptions.accountName.includes("stg.opstg.com") ? `https://${this.cliOptions.accountName}.twingate.com`: `https://${this.cliOptions.accountName}`,
+            tokens = await this.client.generateConnectorTokens(connector.id);
 
         Log.info("Creating Azure Container, please wait.");
 
-        try {
-            //const [code, output, error] = await this.createVm(hostname, cloudConfigFile);
-            if ( code !== 0 ) throw new Error(error);
-            Log.success(`Created connector using Azure Container Service!\n`);
-            return 0;
-        }
-        catch (e) {
-            Log.error(e);
-            throw e;
-        }
+        const instance = await this.createContainer(resourceGroup.name, vnet.name, subnet.name, hostname, options,accountUrl, tokens);
+
+        Log.success(`Created Azure container instance!\n`);
+        const table = new Table();
+        table.push(["Location", instance.location]);
+        table.push([`${instance.ipAddress.type} IP`, instance.ipAddress.ip]);
+        table.render();
     }
 }

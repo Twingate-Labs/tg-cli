58d666ae0feb355b9087d1f46d662011d46fb960
Emrul Islam <emrul@emrul.com>
2022-08-24 14:45:35 +0100

    Improve machineId - return empty in case of error

--- crypto.mjs
+++ crypto.mjs
@@ -4,59 +4,63 @@
 //const base64_to_buf = (b64) => Uint8Array.from(atob(b64), (c) => c.charCodeAt(null));
 
 import {execCmd} from "./utils/smallUtilFuncs.mjs";
+import {Log} from "./utils/log.js";
 
 const enc = new TextEncoder();
 const dec = new TextDecoder();
 
 
 async function machineId() {
-    let guid;
-
-    switch (Deno.build.os) {
-        case "linux": {
-            var output = "";
-            try {
-                output = await execCmd(
-                    ["cat", "/var/lib/dbus/machine-id", "/etc/machine-id"],
-                );
-            } catch {
-                output = await execCmd(["hostname"]);
+    let guid = "";
+    try {
+        switch (Deno.build.os) {
+            case "linux": {
+                var output = "";
+                try {
+                    output = await execCmd(
+                        ["cat", "/var/lib/dbus/machine-id", "/etc/machine-id"],
+                    );
+                } catch {
+                    output = await execCmd(["hostname"]);
+                }
+                guid = output
+                    .substr(0, output.indexOf("\n"))
+                    .replace(/\r+|\n+|\s+/ig, "")
+                    .toLowerCase();
+                break;
             }
-            guid = output
-                .substr(0, output.indexOf("\n"))
-                .replace(/\r+|\n+|\s+/ig, "")
-                .toLowerCase();
-            break;
-        }
 
-        case "darwin": {
-            const output = await execCmd(
-                ["ioreg", "-rd1", "-c", "IOPlatformExpertDevice"],
-            );
-            guid = output.split("IOPlatformUUID")[1]
-                .split("\n")[0].replace(/\=|\s+|\"/ig, "")
-                .toLowerCase();
-            break;
-        }
+            case "darwin": {
+                const output = await execCmd(
+                    ["ioreg", "-rd1", "-c", "IOPlatformExpertDevice"],
+                );
+                guid = output.split("IOPlatformUUID")[1]
+                    .split("\n")[0].replace(/\=|\s+|\"/ig, "")
+                    .toLowerCase();
+                break;
+            }
 
-        case "windows": {
-            const output = await execCmd(
-                [
-                    "REG",
-                    "QUERY",
-                    "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography",
-                    "/v",
-                    "MachineGuid",
-                ],
-            );
-            guid = output
-                .split("REG_SZ")[1]
-                .replace(/\r+|\n+|\s+/ig, "")
-                .toLowerCase();
-            break;
+            case "windows": {
+                const output = await execCmd(
+                    [
+                        "REG",
+                        "QUERY",
+                        "HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography",
+                        "/v",
+                        "MachineGuid",
+                    ],
+                );
+                guid = output
+                    .split("REG_SZ")[1]
+                    .replace(/\r+|\n+|\s+/ig, "")
+                    .toLowerCase();
+                break;
+            }
         }
     }
-
+    catch (e) {
+        Log.warn(`Unable to retrieve machineId.`)
+    }
     return guid;
 }
 
@@ -74,7 +78,7 @@ const deriveKey = (passwordKey, salt, keyUsage) =>
 const IV_SIZE = 16;
 const SALT_SIZE = 16;
 
-export async function encryptData(secretData, password=HW_ID) {
+export async function encryptData(secretData, password = HW_ID) {
 
     const salt = crypto.getRandomValues(new Uint8Array(SALT_SIZE));
     const iv = crypto.getRandomValues(new Uint8Array(IV_SIZE));
@@ -93,7 +97,7 @@ export async function encryptData(secretData, password=HW_ID) {
 
 }
 
-export async function decryptData(encryptedDataBuff, password=HW_ID) {
+export async function decryptData(encryptedDataBuff, password = HW_ID) {
     const salt = encryptedDataBuff.slice(0, SALT_SIZE);
     const iv = encryptedDataBuff.slice(SALT_SIZE, SALT_SIZE + IV_SIZE);
     const data = encryptedDataBuff.slice(SALT_SIZE + IV_SIZE);
